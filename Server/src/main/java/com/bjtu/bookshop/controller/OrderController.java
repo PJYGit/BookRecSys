addabc26d5916ff1be5a8e9b47911af96cca5a6c
董舜尧 <dongshunyao@qq.com>
2020-07-08 01:45:49 +0800

    有问题直接改

--- Server/src/main/java/com/bjtu/bookshop/controller/OrderController.java
+++ Server/src/main/java/com/bjtu/bookshop/controller/OrderController.java
@@ -1,7 +1,17 @@
 package com.bjtu.bookshop.controller;
 
-import org.springframework.stereotype.Controller;
+import com.bjtu.bookshop.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/order")
 public class OrderController {
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
 }
