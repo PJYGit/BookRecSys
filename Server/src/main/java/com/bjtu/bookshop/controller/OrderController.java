caeb0fe10f40bd4b33c4f8f4073e69b39399c74e
董舜尧 <dongshunyao@qq.com>
2020-07-08 01:42:53 +0800

    create order

---
+++ Server/src/main/java/com/bjtu/bookshop/controller/OrderController.java
@@ -0,0 +1,7 @@
+package com.bjtu.bookshop.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class OrderController {
+}
---
+++ Server/src/main/java/com/bjtu/bookshop/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.bjtu.bookshop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+}
