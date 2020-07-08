ce6e92554721c88eaaf83fbde5ee7fa884896d1e
PJYGit <870096160@qq.com>
2020-07-08 16:10:00 +0800

    用户聚类完成，聚类结果见hive的final_result表 TODO:模块结合

--- BigData/BookShopBI/com/ml/cluster/user_cluster.py
+++ BigData/BookShopBI/com/ml/cluster/user_cluster.py
@@ -1,5 +1,8 @@
-import os
+import sys
 
+sys.path.append('/home/software/files/BookShopBI/BookShopBI/')
+
+import os
 from com.utils.HiveUtil import HiveUtil
 from com.utils.py_env import HADOOP_PATH, PROJECT_LIB_DIR, PROJECT_TMP_DIR
 
@@ -21,17 +24,25 @@ def cluster_output():
             + " " + PROJECT_TMP_DIR + "result"
     os.system(shell)
 
+    # 删除聚类结果文件中存留的空行
+    shell = "sed '/^$/d' " + PROJECT_TMP_DIR + "result.txt > " + PROJECT_TMP_DIR + "temp.out"
+    os.system(shell)
+    shell = "rm -rf " + PROJECT_TMP_DIR + "result.txt"
+    os.system(shell)
+    shell = "mv " + PROJECT_TMP_DIR + "temp.out " + PROJECT_TMP_DIR + "result.txt"
+    os.system(shell)
+
     # 将本地的结果文件加载到Hive的cluster_result表中
-    hql = "load data local inpath '" + PROJECT_TMP_DIR + "result overwrite into table cluster_result"
+    hql = "load data local inpath '" + PROJECT_TMP_DIR + "result.txt' overwrite into table cluster_result"
     HiveUtil.execute_shell(hql)
 
 
 def get_finalresult():
-    hql = "insert overwrite table final_result select t2.customerId,t1.* from " \
-          "(select clusterId,subTotal,ordersCount,sessionCount from cluster_result group by clusterId,subTotal,ordersCount,sessionCount) t1 " \
-          "join (select customerId,(subTotal-avg_subTotal)/std_subTotal subTotal,(ordersCount-avg_ordersCount)/std_ordersCount ordersCount," \
-                "(sessionCount-avg_sessionCount)/std_sessionCount sessionCount from user_dimension " \
-                "join (select std(subTotal) std_subTotal,std(ordersCount) std_ordersCount,std(sessionCount) std_sessionCount from user_dimension) t1 on 1=1 " \
-                "join (select avg(subTotal) avg_subTotal,avg(ordersCount) avg_ordersCount,avg(sessionCount) avg_sessionCount from user_dimension) t2 on 1=1) t2 " \
-          "on t1.subTotal=t2.subTotal and t1.ordersCount=t2.ordersCount and t1.sessionCount=t2.sessionCount"
+    hql = "insert overwrite table final_result select t2.customerid,t1.* from " \
+          "(select clusterid,subtotal,orderscount,sessioncount from cluster_result group by clusterid,subtotal,orderscount,sessioncount) t1 " \
+          "join (select customerid,(subtotal-avg_subtotal)/std_subtotal subtotal,(orderscount-avg_orderscount)/std_orderscount orderscount," \
+          "(sessioncount-avg_sessioncount)/std_sessioncount sessioncount from user_dimension " \
+          "join (select std(subtotal) std_subtotal,std(orderscount) std_orderscount,std(sessioncount) std_sessioncount from user_dimension) t1 on 1=1 " \
+          "join (select avg(subtotal) avg_subtotal,avg(orderscount) avg_orderscount,avg(sessioncount) avg_sessioncount from user_dimension) t2 on 1=1) t2 " \
+          "on t1.subtotal=t2.subtotal and t1.orderscount=t2.orderscount and t1.sessioncount=t2.sessioncount"
     HiveUtil.execute_shell(hql)
