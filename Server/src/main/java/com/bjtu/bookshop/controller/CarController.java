708c14121c2c8df36b8e166aed626ff00708d1cd
董舜尧 <dongshunyao@qq.com>
2020-07-10 21:42:27 +0800

    car controller plus

--- Server/src/main/java/com/bjtu/bookshop/controller/CarController.java
+++ Server/src/main/java/com/bjtu/bookshop/controller/CarController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author 李高丞
+ * @author 李高丞、dsy
  */
 
 @RestController
