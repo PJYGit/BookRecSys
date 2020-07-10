69756a7a4f3a312ae1f0b7dc03fec0e3e6545f25
董舜尧 <dongshunyao@qq.com>
2020-07-10 21:34:02 +0800

    clear controller

--- Server/src/main/java/com/bjtu/bookshop/controller/UserController.java
+++ Server/src/main/java/com/bjtu/bookshop/controller/UserController.java
@@ -1,16 +1,10 @@
 package com.bjtu.bookshop.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.bjtu.bookshop.bean.db.UserInfo;
-import com.bjtu.bookshop.bean.request.UserRequests;
 import com.bjtu.bookshop.bean.request.UserRequests.*;
 import com.bjtu.bookshop.bean.response.UserResponses.*;
-import com.bjtu.bookshop.response.Response;
 import com.bjtu.bookshop.service.UserService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +28,9 @@ public class UserController {
      */
     @RequestMapping(value = "/login", method = {RequestMethod.POST})
     public LoginResponse userLogin(@Valid LoginRequest req, BindingResult br) {
-        if(br.hasErrors()) return new LoginResponse(){{setState(-1);}};
+        if (br.hasErrors()) return new LoginResponse() {{
+            setState(-1);
+        }};
         return userService.userLogin(req.getUrn(), req.getPw());
     }
 
@@ -44,8 +40,12 @@ public class UserController {
      */
     @RequestMapping(value = "/logout", method = {RequestMethod.POST})
     public LogoutResponse userLogout(@Valid LogoutRequest req, BindingResult br) {
-        if(br.hasErrors()) return new LogoutResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new LogoutResponse(){{setState(-10);}};
+        if (br.hasErrors()) return new LogoutResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new LogoutResponse() {{
+            setState(-10);
+        }};
         return userService.userLogout(req.getUid());
     }
 
@@ -55,9 +55,11 @@ public class UserController {
      */
     @RequestMapping(value = "/register", method = {RequestMethod.POST})
     public RegisterResponse userRegister(@Valid RegisterRequest req, BindingResult br) {
-        if(br.hasErrors()) return new RegisterResponse(){{setState(-1);}};
-        if(req.getUname() == null || req.getUname().isEmpty()) req.setUname(req.getUrn());
-        return userService.registerUser(req.getUrn(), req.getUname(), req.getPsw(),req.getCode());
+        if (br.hasErrors()) return new RegisterResponse() {{
+            setState(-1);
+        }};
+        if (req.getUname() == null || req.getUname().isEmpty()) req.setUname(req.getUrn());
+        return userService.registerUser(req.getUrn(), req.getUname(), req.getPsw(), req.getCode());
     }
 
     /**
@@ -66,7 +68,9 @@ public class UserController {
      */
     @RequestMapping(value = "/phone", method = {RequestMethod.POST})
     public PhoneResponse userPhone(@Valid PhoneRequest req, BindingResult br) {
-        if(br.hasErrors()) return new PhoneResponse(){{setState(-1);}};
+        if (br.hasErrors()) return new PhoneResponse() {{
+            setState(-1);
+        }};
         return new PhoneResponse(0);
     }
 
@@ -76,8 +80,12 @@ public class UserController {
      */
     @RequestMapping(value = "/getinfo", method = {RequestMethod.POST})
     public GetInfoResponse getUserInfo(@Valid GetInfoRequest req, BindingResult br) {
-        if(br.hasErrors()) return new GetInfoResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new GetInfoResponse(){{setState(-10);}};
+        if (br.hasErrors()) return new GetInfoResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new GetInfoResponse() {{
+            setState(-10);
+        }};
         return userService.getUserInfo(req.getUid());
     }
 
@@ -87,11 +95,17 @@ public class UserController {
      */
     @RequestMapping(value = "/setinfo", method = {RequestMethod.POST})
     public SetInfoResponse updateUserInfo(@Valid SetInfoRequest req, BindingResult br) {
-        if(br.hasErrors()) return new SetInfoResponse(){{setState(-1);}};
-        if(! req.trans()) return new SetInfoResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new SetInfoResponse(){{setState(-10);}};
+        if (br.hasErrors()) return new SetInfoResponse() {{
+            setState(-1);
+        }};
+        if (!req.trans()) return new SetInfoResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new SetInfoResponse() {{
+            setState(-10);
+        }};
         return userService.updateUserInfo(
-                req.getUid(),req.getNickname(),req.getHead(),req.getInnerAddress());
+                req.getUid(), req.getNickname(), req.getHead(), req.getInnerAddress());
     }
 
     /**
@@ -100,9 +114,15 @@ public class UserController {
      */
     @RequestMapping(value = "/manage/list", method = {RequestMethod.POST})
     public ManageListResponse getUserList(@Valid ManageListRequest req, BindingResult br) {
-        if(br.hasErrors()) return new ManageListResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new ManageListResponse(){{setState(-10);}};
-        if(! userService.checkUserRole(req.getUid(),1)) return new ManageListResponse(){{setState(-10);}};
+        if (br.hasErrors()) return new ManageListResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new ManageListResponse() {{
+            setState(-10);
+        }};
+        if (!userService.checkUserRole(req.getUid(), 1)) return new ManageListResponse() {{
+            setState(-10);
+        }};
         return userService.getUserList(req.getPage() == null ? 0 : req.getPage());
     }
 
@@ -112,9 +132,15 @@ public class UserController {
      */
     @RequestMapping(value = "/manage/search", method = {RequestMethod.POST})
     public ManageSearchResponse searchUser(@Valid ManageSearchRequest req, BindingResult br) {
-        if(br.hasErrors()) return new ManageSearchResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new ManageSearchResponse(){{setState(-10);}};
-        if(! userService.checkUserRole(req.getUid(),1)) return new ManageSearchResponse(){{setState(-10);}};
+        if (br.hasErrors()) return new ManageSearchResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new ManageSearchResponse() {{
+            setState(-10);
+        }};
+        if (!userService.checkUserRole(req.getUid(), 1)) return new ManageSearchResponse() {{
+            setState(-10);
+        }};
         return userService.searchUserWithPhone(req.getPhone());
     }
 
@@ -124,9 +150,15 @@ public class UserController {
      */
     @RequestMapping(value = "/manage/getinfo", method = {RequestMethod.POST})
     public ManageGetResponse getUserInfoM(@Valid ManageGetRequest req, BindingResult br) {
-        if(br.hasErrors()) return new ManageGetResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new ManageGetResponse(){{setState(-10);}};
-        if(! userService.checkUserRole(req.getUid(),1)) return new ManageGetResponse(){{setState(-10);}};
+        if (br.hasErrors()) return new ManageGetResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new ManageGetResponse() {{
+            setState(-10);
+        }};
+        if (!userService.checkUserRole(req.getUid(), 1)) return new ManageGetResponse() {{
+            setState(-10);
+        }};
         return userService.getManagedUserInfo(req.getTarget());
     }
 
@@ -136,11 +168,17 @@ public class UserController {
      */
     @RequestMapping(value = "/manage/setinfo", method = {RequestMethod.POST})
     public ManageSetResponse modifyUserInfo(@Valid ManageSetRequest req, BindingResult br) {
-        if(br.hasErrors()) return new ManageSetResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new ManageSetResponse(){{setState(-10);}};
-        if(! userService.checkUserRole(req.getUid(),1)) return new ManageSetResponse(){{setState(-10);}};
-        return userService.modifyUserInfo(req.getUid(), req.getTarget(),req.getNickname(),req.getVipRate(),
-                req.getBaned(),req.getMoney(),req.getRole());
+        if (br.hasErrors()) return new ManageSetResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new ManageSetResponse() {{
+            setState(-10);
+        }};
+        if (!userService.checkUserRole(req.getUid(), 1)) return new ManageSetResponse() {{
+            setState(-10);
+        }};
+        return userService.modifyUserInfo(req.getUid(), req.getTarget(), req.getNickname(), req.getVipRate(),
+                req.getBaned(), req.getMoney(), req.getRole());
     }
 
     /**
@@ -149,10 +187,16 @@ public class UserController {
      */
     @RequestMapping(value = "/manage/adduser", method = {RequestMethod.POST})
     public ManageAddResponse addUser(@Valid ManageAddRequest req, BindingResult br) {
-        if(br.hasErrors()) return new ManageAddResponse(){{setState(-1);}};
-        if(! userService.checkUserToken(req)) return new ManageAddResponse(){{setState(-10);}};
-        if(! userService.checkUserRole(req.getUid(),1)) return new ManageAddResponse(){{setState(-10);}};
+        if (br.hasErrors()) return new ManageAddResponse() {{
+            setState(-1);
+        }};
+        if (!userService.checkUserToken(req)) return new ManageAddResponse() {{
+            setState(-10);
+        }};
+        if (!userService.checkUserRole(req.getUid(), 1)) return new ManageAddResponse() {{
+            setState(-10);
+        }};
         return userService.addUser(req.getUrn(), req.getPassword(), req.getNickname(),
-                req.getVipRate(), req.getRole(), req.getBaned(),req.getMoney());
+                req.getVipRate(), req.getRole(), req.getBaned(), req.getMoney());
     }
 }
