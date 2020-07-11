b62f6eb932d6ef7ae2ad758a4190ff9b97615979
GaochengLee <460270786@qq.com>
2020-07-11 10:38:30 +0800

    店铺管理界面增加

--- client/api/API_PRO.js
+++ client/api/API_PRO.js
@@ -3,7 +3,7 @@ export default {
     //baseURL: 'http://134.175.237.140:9000/',//后端地址
     //baseURL: 'http://127.0.0.1:8080/',//后端地址
     baseURL: 'http://39.106.160.119:9000/',
-    
+
     method: 'post',
     //登录模块接口
     userLogin: {url: 'user/login'},
@@ -17,10 +17,20 @@ export default {
     editCarOrder: {url: '/car/setlist'},
     submitCarOrder: {url: '/car/submit'},
 
-    //个人中心模块
-    changeMsg:{url:'user/setinfo'},
+    // 店铺模块
+    getShopBookInfo: {url: '/shop/booklist'},
 
-    //管理模块
-    getUSList:{url:'user/manage/list'},//获取用户列表
-    setUserInfo:{url:'user/manage/setinfo'}//修改用户信息
+    //个人中心模块
+    changeMsg: {url: 'user/setinfo'},
+
+    // 个人管理模块
+    getUSList: {url: 'user/manage/list'},//获取用户列表
+    setUserInfo: {url: 'user/manage/setinfo'},//修改用户信息
+
+    // 店铺管理模块
+    getShopInfo: {url: '/shop/manage/getinfo'},
+    setShopInfo: {url: '/shop/manage/setinfo'},
+    addNewBookIntoShop: {url: '/shop/manage/addbook'},
+    setBookInfo: {url: '/shop/manage/setbookinfo'},
+    delBookInfo: {url: '/shop/manage/delbook'},
 }
