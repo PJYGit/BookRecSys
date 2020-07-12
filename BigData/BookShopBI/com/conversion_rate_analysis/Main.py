19c9ea43d5be5d63d12a351dcb9de387c58f43d0
PJYGit <870096160@qq.com>
2020-07-12 11:51:42 +0800

    Analysis 收尾

--- BigData/BookShopBI/com/conversion_rate_analysis/Main.py
+++ BigData/BookShopBI/com/conversion_rate_analysis/Main.py
@@ -14,13 +14,13 @@ if __name__ == '__main__':
     end = sys.argv[2]
 
     # 解析配置文件
-    urls = buildConversion.resolve_conf(0, "2019-05-28")
+    urls = buildConversion.resolve_conf(0, "")
 
     # 提取所需数据
-    # buildConversionInput.extract_data(start, end)
+    buildConversionInput.extract_data(start, end)
 
     # 通过MapReduce进行统计
     buildConversionHadoopShell.count_urls(start, end, urls)
 
     # 对中间结果进行汇总并得到最后结果表
-    # buildConversionResult.get_result(start, end, "/user/warehouse/conversion_out")
+    buildConversionResult.get_result(start, end, "/user/warehouse/conversion_out")
--- BigData/BookShopBI/com/conversion_rate_analysis/buildConversionResult.py
+++ BigData/BookShopBI/com/conversion_rate_analysis/buildConversionResult.py
@@ -13,15 +13,15 @@ def get_result(start, end, output):
     dt = start + "-" + end
 
     # 删除作业成功的标志文件和作业的日志文件
-    shell = HADOOP_PATH + "hadoop fs -rm -rf " + output + "/_SUCCESS"
+    shell = HADOOP_PATH + "hadoop fs -rm -f " + output + "/_SUCCESS"
     os.system(shell)
-    shell = HADOOP_PATH + "hadoop fs -rm " + output + "/_logs"
-    os.system(shell)
+    # shell = HADOOP_PATH + "hadoop fs -rm " + output + "/_logs"
+    # os.system(shell)
 
     # 将临时结果加载到中间结果表
     hql = "load data inpath '" + output + "' overwrite into table conversion_middle_result partition (dt=' + dt + ')"
     HiveUtil.execute_shell(hql)
 
     # 对中间结果进行汇总，并写入到最终结果表
-    hql = "insert into table conversion_result partition (dt='" + start + "-" + end + "') select process,count(process),count(distinct(uuid)) from conversion_middle_result where dt = " + dt + " group by process"
+    hql = "insert into table conversion_result partition (dt='" + start + "-" + end + "') select process,count(process),count(distinct(uuid)) from conversion_middle_result group by process"
     HiveUtil.execute_shell(hql)
